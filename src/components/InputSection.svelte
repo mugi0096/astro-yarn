69b678ab826284be9e3cf6836b96b5c8c491e4a1
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-09-18 14:48:33 +0900

    ✨ add about page

---
+++ src/components/InputSection.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let inputValue = "";
+</script>
+
+<input bind:value={inputValue} placeholder="8文字まで入力可" maxlength="8" class="outline rounded outline-1 outline-violet-400 p-1" />
+<p>入力された文字列: {inputValue || ""}</p>
