2d59e10d133e175ab16a379ecf2515ee396413b7
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-09-18 14:50:43 +0900

    ✨ add error simulation page

---
+++ src/components/OccurError.svelte
@@ -0,0 +1,8 @@
+<script>
+  const throwError = () => {
+   throw new Error('simulate error');
+  }
+ </script>
+ 
+ <button on:click={throwError} class="rounded p-2 bg-violet-400">throw error</button>
+  
